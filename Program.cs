f31a09851d3a74d52374f52ccc69aac1a2db3a2a
339780520 <339780520@2260I-L29-008>
2025-11-05 08:45:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12_T7_Lists_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
